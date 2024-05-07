47d3900422132dae944df27413be3da6bf3df8cd
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-05-07 14:06:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace adonet_db_videogame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
